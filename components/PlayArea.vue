2a33858c5a14339392fe4c191237b93d35dcf0ab
Yuuki Tanoue <Tanoue@you-no-MacBook-Air.local>
2019-06-18 17:31:44 +0900

    create

--- components/PlayArea.vue
+++ components/PlayArea.vue
@@ -7,15 +7,20 @@
       template(v-for="(area, i) in playArea")
         template(v-for="(mino, j) in area")
           rect(v-if="mino!==-1"
-            :x="i * blockSize"
-            :y="j * blockSize"
+            :y="i * blockSize"
+            :x="j * blockSize"
             :width="blockSize"
             :height="blockSize"
             :fill="getColor(mino)"
-            stroke="white"
-            stroke-width="3")
+            stroke="black"
+            stroke-width="1")
+    b-button(@click="left" variant="warning") 左
+    b-button(@click="right" variant="danger") 右
+    b-button(@click="down" variant="primary") 下
+    b-button(@click="rotate") 回転
 </template>
 <script lang="ts">
+import { setInterval } from 'timers'
 import Vue from 'vue'
 import PlayAreaModule from '@/store/modules/play'
 
@@ -23,7 +28,7 @@ export default Vue.extend({
   data() {
     return {
       // 1マスのサイズ[px]
-      blockSize: 50
+      blockSize: 40
     }
   },
   computed: {
@@ -38,9 +43,27 @@ export default Vue.extend({
       return PlayAreaModule.getPlayArea
     }
   },
+  mounted() {
+    PlayAreaModule.initNextMinoList()
+    PlayAreaModule.startPlay()
+  },
   methods: {
+    right() {
+      PlayAreaModule.startPlay()
+    },
+    left() {
+      PlayAreaModule.moveLeft()
+    },
+    down() {
+      setInterval(() => PlayAreaModule.moveDown(), 1000)
+    },
+    rotate() {
+      PlayAreaModule.rotate()
+    },
     getColor(index: number): string {
       switch (index) {
+        case 0:
+          return 'white'
         case 1:
           return 'red'
         case 2:
--- components/types/TetoMino.d.ts
+++
@@ -1,5 +0,0 @@
-export default interface Tetromino {
-  name: string
-  color: string
-  blocks: number[][][]
-}
